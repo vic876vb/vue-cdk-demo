a48ab356e1d690a7ba245c389b17de6a7d851c3b
Victor Antonio Hernandez Nuñez <victor.hernandezn@outlook.com>
2024-01-30 18:44:39 +0100

    Initial commit

---
+++ lib/vue-cdk-demo-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class VueCdkDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'VueCdkDemoQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
